9c3ad55da36a32e754d5c1ad74d7dfbdc97fd28a
Colin <colin.diesh@gmail.com>
2021-01-16 08:20:59 -0700

    Updates

---
+++ precache-manifest.271a4244b873e255fd14e8e3f1a7fc5f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fdf26672362524c62983c12ea29d7af",
+    "url": "./index.html"
+  },
+  {
+    "revision": "975fdd30f8be41c6cf88",
+    "url": "./static/css/main.4802a72b.chunk.css"
+  },
+  {
+    "revision": "7565f1d643beec57d7ae",
+    "url": "./static/js/2.5edd3249.chunk.js"
+  },
+  {
+    "revision": "b338715ae810620e92f535ad7bcf2498",
+    "url": "./static/js/2.5edd3249.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "975fdd30f8be41c6cf88",
+    "url": "./static/js/main.ae34788d.chunk.js"
+  },
+  {
+    "revision": "dcbf44a1f017c9600bf2",
+    "url": "./static/js/runtime-main.a90d1d87.js"
+  }
+]);
